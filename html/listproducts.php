ea87a37c15514ddd87f96775ec2b3b833b278851
mponce622 <150394988+mponce622@users.noreply.github.com>
2023-12-01 17:48:17 -0500

    Update listproducts.php

--- html/listproducts.php
+++ html/listproducts.php
@@ -1,48 +1,38 @@
-<?php
-    //include 'connect.php';
-	$servername = "localhost";
-	$username = "admin";
-	$password = "rootroot";
-	$dbname = "inventory";
-			
-	$conn = mysqli_connect($servername, $username, $password, $dbname) or die("bad connection: ".mysqli_connect_error());
-    	$sql = "SELECT p_id, p_name, p_qty FROM inventory";
-	
-	$result = $conn->query($sql);
-	if ($result->num_rows >  0) {
-		while ($row=$result->fetch_assoc()){
-			echo $row["p_id"]. " ".$row["p_name"]." ".$row["p_qty"];
-		echo"<br><br>";
-		}
-    	}
-	else{
-		echo "Empty";
-	}
-	
-?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Website</title>
-    <link rel="stylesheet" href="./style.css">
+    <title>Inventory</title>
   </head>
   <body>
     <main>
-        <h1>Welcome to My Website</h1>
-        <div class="item">
-            <span>Item ID:</span>
-            <span id="item-id">12345</span>
-        </div>
-        <div class="item">
-            <span>Item Name:</span>
-            <span id="item-name">Example Item</span>
-        </div>
-        <div class="item">
-            <span>Qty:</span>
-            <span id="item-qty">10</span>
+        <h1>Inventory List</h1>
+        <form action="listproducts.php" method="post">
+        	<!--Item ID-->
+	<?php
+    	//include 'connect.php';
+		$servername = "localhost";
+		$username = "root";
+		$password = "";
+		$dbname = "inventory";
+			
+		$conn = mysqli_connect($servername, $username, $password, $dbname) or die("bad connection: ".mysqli_connect_error());
+		$sql = "SELECT p_id, p_name, p_quantity FROM inventory;";
+	
+		$result = $conn->query($sql);
+		if ($result->num_rows >  0) {
+			while ($row=$result->fetch_assoc()){
+				echo $row["p_id"]. " ".$row["p_name"]." ".$row["p_quantity"];
+			echo"<br><br>";
+			}
+    		}
+		else{
+			echo "Empty";
+		}
+	
+	?>
         </form>
     </main>
   </body>
