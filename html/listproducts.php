a306c3d166e50d1f535c3b6e1a2a4088819762c5
Joshua Stokes <joshua.stokes.19@cnu.edu>
2023-12-01 16:54:55 -0500

    listproducts change

--- html/listproducts.php
+++ html/listproducts.php
@@ -1,11 +1,11 @@
 <html>
 <?php
-
-    $sql = "SELECT id, name, qty FROM item";
+    include 'connect.php';
+    $sql = "SELECT p_id, p_name, p_qty FROM inventory";
 	$result = $conn->query($sql);
 	if ($result->num_rows >  0) {
 		while ($row=$result->fetch_assoc()){
-			echo $row["id"]. " ".$row["name"]." ".$row["qty"];
+			echo $row["p_id"]. " ".$row["p_name"]." ".$row["p_qty"];
 		echo"<br><br>;
 		}
 	else{
