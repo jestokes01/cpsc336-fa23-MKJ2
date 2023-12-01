3bd98cf11182cdf22c21a8ffd0e700f2f10ab512
Joshua Stokes <joshua.stokes.19@cnu.edu>
2023-12-01 18:24:11 -0500

    change

--- html/listproducts.php
+++ html/listproducts.php
@@ -23,11 +23,14 @@
 	
 		$result = $conn->query($sql);
 		if ($result->num_rows >  0) {
+			echo "<table border='1'>";
 			while ($row=$result->fetch_assoc()){
-				echo $row["p_id"]. " ".$row["p_name"]." ".$row["p_quantity"];
+				echo "<td style='border: 1px solid black; padding: 5px; '>".$row["p_id"]."</td>";
+				echo "<td style='border: 1px solid black; padding: 5px; '>".$row["p_name"]."</td>";
+				echo "<td style='border: 1px solid black; padding: 5px; '>".$row["p_quantity"]."</td>";
 			echo"<br><br>";
 			}
-    		}
+    	}
 		else{
 			echo "Empty";
 		}
