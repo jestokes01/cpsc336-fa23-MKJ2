4198408dfe93ca456a094d745bf9365d1a3e3e75
Joshua Stokes <joshua.stokes.19@cnu.edu>
2023-12-01 17:16:46 -0500

    change

--- html/listproducts.php
+++ html/listproducts.php
@@ -1,7 +1,14 @@
 <html>
 <?php
-    include 'connect.php';
+    //include 'connect.php';
+	$servername = "localhost";
+	$username = "admin";
+	$password = "rootroot";
+	$dbname = "inventory";
+			
+	$conn = mysqli_connect($servername, $username, $password, $dbname) or die("bad connection: ".mysqli_connect_error());
     $sql = "SELECT p_id, p_name, p_qty FROM inventory";
+	
 	$result = $conn->query($sql);
 	if ($result->num_rows >  0) {
 		while ($row=$result->fetch_assoc()){
