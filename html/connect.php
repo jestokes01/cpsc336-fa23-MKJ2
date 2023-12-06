145eda94d8a29b02f5c0a4bd1bcfbbe33f6f005c
mponce622 <150394988+mponce622@users.noreply.github.com>
2023-12-06 15:39:09 -0500

    Delete html/connect.php

--- html/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$servername = "localhost";
-	$username = "admin";
-	$password = "rootroot";
-	$dbname = "inventory";
-			
-	$conn = mysqli_connect($servername, $username, $password, $dbname) or die("bad connection: ".mysqli_connect_error());
-?>
