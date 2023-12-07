a31c9aea6ab6441ceb85a3ee8da10f8546dc1fa3
mponce622 <150394988+mponce622@users.noreply.github.com>
2023-12-07 15:09:19 -0500

    Production version of connect.php

--- html/connect.php
+++ html/connect.php
@@ -1,16 +1,21 @@
 <?php
 	function db_connect()
 	{
+	//DEVELOPMENT
 	//$servername = "localhost";
-	//$username = "admin";
-	//$password = "rootroot";
+	//$username = "root";
+	//$password = "";
 	//$dbname = "inventory";
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "inventory";
 			
-	$conn = mysqli_connect($servername, $username, $password, $dbname) or die("bad connection: ".mysqli_connect_error());
+	//$conn = mysqli_connect($servername, $username, $password, $dbname) or die("bad connection: ".mysqli_connect_error());
+	
+	$servername = $_SERVER['RDS_HOSTNAME'];
+	$username = $_SERVER['RDS_USERNAME'];
+	$password = $_SERVER['RDS_PASSWORD'];
+	$dbname = $_SERVER['RDS_DB_NAME'];
+	$port = $_SERVER['RDS_PORT'];
+	
+	$conn = new mysqli($servername, $username, $password, $dbname, $port) or die("bad connection: ".mysqli_connect_error());
 	return $conn;
 	}
 	
