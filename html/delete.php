b54e7a83a36ce3e5e1d0947e9007753ce9dce5c5
mponce622 <150394988+mponce622@users.noreply.github.com>
2023-12-06 21:41:24 -0500

    Final touches on delete.php

--- html/delete.php
+++ html/delete.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Inventory</title>
+    <title>Delete Inventory</title>
   </head>
   <body>
     <main>
@@ -13,20 +13,16 @@
         	<!--Item ID-->
         	<label for="ItemID">Item ID</label><br>
         	<input name="itemid" id="itemid" type="text" p_id="Item ID"><br><br>
+
         	<!--Delete Item Button-->
         	<input type="submit" value="Delete Item">
           <?php
-            	//include 'connect.php';
-        		$servername = "localhost";
-        		$username = "root";
-        		$password = "";
-        		$dbname = "inventory";
-
-        		$conn = mysqli_connect($servername, $username, $password, $dbname) or die("bad connection: ".mysqli_connect_error());
+            include 'connect.php';
+            $conn = db_connect();
         		$sql = "DELETE FROM inventory WHERE p_id = ".$_POST["itemid"].";";
         		echo $sql;
         		$result = $conn->query($sql);
-
+            db_close($conn);
         	?>
         </form>
     </main>
