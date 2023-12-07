13010872419a513128698fac1617f7c8979e1c6b
mponce622 <150394988+mponce622@users.noreply.github.com>
2023-12-07 15:18:12 -0500

    Test 2 of final connect.php

--- html/connect.php
+++ html/connect.php
@@ -9,11 +9,13 @@
 			
 	//$conn = mysqli_connect($servername, $username, $password, $dbname) or die("bad connection: ".mysqli_connect_error());
 	
-	$servername = $_SERVER['RDS_HOSTNAME'];
-	$username = $_SERVER['RDS_USERNAME'];
-	$password = $_SERVER['RDS_PASSWORD'];
-	$dbname = $_SERVER['RDS_DB_NAME'];
-	$port = $_SERVER['RDS_PORT'];
+	//endpoint
+	$servername = 'productiondb.cvfvx02sqzrk.us-east-1.rds.amazonaws.com';
+	//admin
+	$username = 'admin';
+	$password = 'rootroot';
+	$dbname = 'inventory';
+	$port = 3306;
 	
 	$conn = new mysqli($servername, $username, $password, $dbname, $port) or die("bad connection: ".mysqli_connect_error());
 	return $conn;
